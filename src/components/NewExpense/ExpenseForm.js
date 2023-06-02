b07bcf2f899b4c1d7d12dc412a8051e52dd48ecf
CyberManMatt <mtthwjarrett0@gmail.com>
2023-06-02 13:17:29 -0400

    Added filter year to ExpenseDisplay

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,36 +1,46 @@
 import "./ExpenseForm.css"
 import {useState} from "react";
 
-export function ExpenseForm() {
+export function ExpenseForm(props) {
 
-    const [enteredTitle, setEnteredTitle] = useState("")
-    const [enteredAmount, setEnteredAmount] = useState("")
-    const [enteredDate, setEnteredDate] = useState("")
+    // Initialize state for title, amount, and date
+    const [enteredTitle, setEnteredTitle] = useState("");
+    const [enteredAmount, setEnteredAmount] = useState("");
+    const [enteredDate, setEnteredDate] = useState("");
 
+    // When the user starts typing the title in the form
     const titleChangeHandler = (event) => {
         setEnteredTitle(event.target.value);
     };
 
+    // When the user changes the amount in the form
     const amountChangeHandler = (event) => {
         setEnteredAmount(event.target.value);
     };
 
+    // When the user changes the date in the form
     const dateChangeHandler = (event) => {
         setEnteredDate(event.target.value);
     };
 
+    // When the user submits the form
     const submitHandler = (event) => {
+
+        // This prevents the default behavior for submitting a form, which is sending the POST data directly to the server.
         event.preventDefault();
 
+        // Create an expenseData object
         const expenseData = {
             title: enteredTitle,
             amount: enteredAmount,
             date: new Date(enteredDate)
         };
-        console.log(expenseData)
-        setEnteredTitle("")
-        setEnteredDate("")
-        setEnteredAmount("")
+
+        // Custom event handler that is called from the NewExpense component as a prop
+        props.onSaveExpenseData(expenseData);
+        setEnteredTitle("");
+        setEnteredDate("");
+        setEnteredAmount("");
     };
 
     return (
@@ -53,5 +63,5 @@ export function ExpenseForm() {
                 <button type="submit">Add Expense</button>
             </div>
         </form>
-    )
+    );
 }
--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,10 +1,24 @@
 import "./NewExpense.css";
 import {ExpenseForm} from "./ExpenseForm";
 
-export function NewExpense() {
+export function NewExpense(props) {
+
+    // Custom event handler that gets called when the user submits the new Expense form
+    const saveExpenseDataHandler = (expenseData) => {
+
+        // Add data from the ExpenseForm to the object below, and add an ID field
+        const data = {
+            ...expenseData,
+            id: Math.random().toString()
+        };
+
+        // Event handler gets called from App.js component when the user submits the New Expense form.
+        props.onAddExpense(data)
+    };
+
     return (
         <div className="new-expense">
-            <ExpenseForm />
+            <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
         </div>
-    )
+    );
 }
