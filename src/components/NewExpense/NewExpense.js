de8666ede6a4f5ea04e094d93258e838a08a52a4
CyberManMatt <mtthwjarrett0@gmail.com>
2023-06-03 12:09:47 -0400

    Added button to add new expense
    - Modified NewExpense.js to show a button to add a new expense
    
    - Modified ExpenseForm.js to add a Cancel button to cancel adding a new expense

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,7 +1,11 @@
 import "./NewExpense.css";
 import {ExpenseForm} from "./ExpenseForm";
+import {useState} from "react";
 
 export function NewExpense(props) {
+
+    // Initialize state for the new expense form
+    const [isEditing, setIsEditing] = useState(false)
 
     // Custom event handler that gets called when the user submits the new Expense form
     const saveExpenseDataHandler = (expenseData) => {
@@ -16,9 +20,24 @@ export function NewExpense(props) {
         props.onAddExpense(data)
     };
 
+    // Custom handler that opens the form when the user presses the Add New Expense button
+    const startEditingHandler = () => {
+        setIsEditing(true);
+    };
+
+    // Custom handler for when the user clicks the cancel button on the form
+    const stopEditingHandler = () => {
+        setIsEditing(false);
+    };
+
     return (
         <div className="new-expense">
-            <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+            {
+                /* If isEditing is true, display form, else display the Add New Expense button */
+                isEditing === true ?
+                <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} onCancel={stopEditingHandler} /> :
+                <button onClick={startEditingHandler}>Add New Expense</button>
+            }
         </div>
     );
 }
